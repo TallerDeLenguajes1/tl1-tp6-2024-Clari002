eedf9f178c23ec2e2ca5a706539405f4302a92f2
Claribel Diaz <128192485+Clari002@users.noreply.github.com>
2024-05-20 14:30:45 -0300

    Agregar Ejercicio2 en CalculadoraV1

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ Console.WriteLine("valor de b:"+b);
 
 //punto 1
 int num;
-string s = "245";
 Console.WriteLine("Ingrese un numero: ");
 if (!int.TryParse(Console.ReadLine(),out num))
 {
@@ -25,3 +24,62 @@ if (!int.TryParse(Console.ReadLine(),out num))
     }
     Console.WriteLine("Valor invertido: "+ numInvertido);
 }
+
+//punto 2
+int n1, n2;
+Console.WriteLine("Ingrese primer numero: ");
+if (!int.TryParse(Console.ReadLine(), out n1))
+{
+    Console.WriteLine("El valor que ha ingresado no es un numero");
+}else{
+    Console.WriteLine("Ingrese segundo numero: ");
+    if (!int.TryParse(Console.ReadLine(), out n2))
+    {
+        Console.WriteLine("El valor que ha ingresado no es un numero");   
+    }else{
+        int valor;
+        Console.WriteLine("-------CALCULADORA-------");
+        Console.WriteLine("(1)Sumar");
+        Console.WriteLine("(2)Restar");
+        Console.WriteLine("(3)Multiplicar");
+        Console.WriteLine("(4)Division");
+        Console.WriteLine("(0)Salir");
+        Console.WriteLine("Ingresar valor: ");
+        if (!int.TryParse(Console.ReadLine(), out valor))
+        {
+            Console.WriteLine("El valor que ha ingresado no es un numero");   
+        }else{
+
+            switch (valor)
+            {
+                case 1:
+                int suma;
+                suma = n1+n2;
+                Console.WriteLine("Suma: "+ suma);
+                break;
+                case 2:
+                int resta;
+                resta=n1-n2;
+                Console.WriteLine("Resta: "+ resta);
+                break;
+                case 3:
+                int producto;
+                producto=n1*n2;
+                Console.WriteLine("Multiplicacion: "+ producto);
+                break;
+                case 4:
+                int division;
+                if (n2!=0)
+                {
+                    division=n1/n2;
+                    Console.WriteLine("Division: "+ division);
+                }
+                break;
+                default:
+                break;
+            }
+        }
+    }
+        
+}
+
